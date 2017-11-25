4dde0573f1fdd48284b14eacfa060b1b57421fc3
Daniel Deutsch <deudan1010@gmail.com>
2017-11-25 22:07:19 +0100

    update data prep

--- src/Plot.jsx
+++ src/Plot.jsx
@@ -9,18 +9,52 @@ import {
 } from "react-vis";
 import * as data from "./data/Marriages.json";
 
-let preparedData = [];
+let data1534 = [];
+let data3554 = [];
+let data55100 = [];
 
-data.data.forEach(element => {
-  preparedData.push({x:element.REF_YEAR, y:element.values[0]['WIVES']});
-});
-console.log(preparedData);
+for (let i = 0; i < data.data.length / 3; i++) {
+  let element = data.data[i];
+  data1534.push({
+    x: element.REF_YEAR,
+    y: element.values[0]["WIVES"] + element.values[0]["HUSBANDS"]
+  });
+}
+
+console.warn(data1534);
+for (let j = 0; j < data1534.length; j++) {
+  for (let k = j + 1; k < data1534.length; k++) {
+    if (data1534[j].x === data1534[k].x) {
+      data1534[j] = { x: data1534[j].x, y: data1534[j].y + data1534[k].y };
+      data1534.splice(k, 1);
+    }
+  }
+}
+
+console.log(data1534.map(el => el.x));
+console.log(data1534);
+
+// data.data.forEach(element => {
+//   if (element.AGE_AT_MARRIAGE === "15 to 19 years old") {
+//     data1534.push({
+//       x: element.REF_YEAR,
+//       y: element.values[0]["WIVES"] + element.values[0]["HUSBANDS"]
+//     });
+//   }
+// });
+
+// console.log(data.data.length);
+
+// const names = data.data.map(el => el.AGE_AT_MARRIAGE);
+// const uniqueArray = names.filter(function(item, pos) {
+//   return names.indexOf(item) == pos;
+// })
+// console.log(uniqueArray);
 
 const years = [];
-preparedData.map(el => {years.push(el.x)});
-
-console.warn(years);
-
+data1534.map(el => {
+  years.push(el.x);
+});
 
 export default class Plot extends React.Component {
   render() {
@@ -28,9 +62,9 @@ export default class Plot extends React.Component {
       <XYPlot width={800} height={800}>
         <HorizontalGridLines />
         <VerticalGridLines />
-        <MarkSeries className="mark-series-example" data={preparedData} />
-        <XAxis top={0} hideTicks tickValues={years} title="X"/>
-        <XAxis title="Year" tickFormat={v => v}/>
+        <MarkSeries className="mark-series-example" data={data1534} />
+        <XAxis top={0} hideTicks tickValues={years} title="X" />
+        <XAxis title="Year" tickFormat={v => v} />
         <YAxis title="Number of Marriages" />
       </XYPlot>
     );
