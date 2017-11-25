037b31eb3f0fdff856cf554c2c594df6551d7a0f
Daniel Deutsch <deudan1010@gmail.com>
2017-11-25 16:45:14 +0100

    update style

--- src/Plot.jsx
+++ src/Plot.jsx
@@ -10,11 +10,11 @@ import {
 } from "react-vis";
 
 const myData = [
-  { x: 1, y: 10, size: 30 },
-  { x: 1.7, y: 12, size: 10 },
-  { x: 2, y: 5, size: 1 },
-  { x: 3, y: 15, size: 12 },
-  { x: 2.5, y: 7, size: 4 }
+  { x: 1, y: 10 },
+  { x: 1.7, y: 12 },
+  { x: 2, y: 5 },
+  { x: 3, y: 15 },
+  { x: 2.5, y: 7 }
 ];
 
 export default class Plot extends React.Component {
@@ -25,7 +25,6 @@ export default class Plot extends React.Component {
         <VerticalGridLines />
         <MarkSeries
           className="mark-series-example"
-          sizeRange={[5, 15]}
           data={myData}
         />
         <XAxis title="Year" />
