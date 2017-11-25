554e73d84fbde393471f76c3e711e685389684a2
Daniel Deutsch <deudan1010@gmail.com>
2017-11-25 16:28:24 +0100

    remove old data, add new statistic and clear base

--- src/RegressionComponent.jsx
+++ src/RegressionComponent.jsx
@@ -1,14 +1,34 @@
 import React from "react";
 
 
-const RegressionComponent = () => {
+class RegressionComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  return (
-    <div>
-    HI
-    </div>
-  );
-};
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Number:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
 
 export default RegressionComponent;
