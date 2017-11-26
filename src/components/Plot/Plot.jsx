baf7313c1025877c950e7b76f07185abd26b65a0
Daniel Deutsch <deudan1010@gmail.com>
2017-11-26 15:26:13 +0100

    outsource helper function

--- src/components/Plot/Plot.jsx
+++ src/components/Plot/Plot.jsx
@@ -7,37 +7,7 @@ import {
   XAxis,
   YAxis
 } from "react-vis";
-import * as data from "../../data/Marriages.json";
-
-
-let preparedData = [];
-for (let i = 0; i < data.data.length; i++) {
-  let element = data.data[i];
-  preparedData.push({
-    x: element.REF_YEAR,
-    y: element.values[0]["WIVES"] + element.values[0]["HUSBANDS"]
-  });
-}
-
-let temp = {};
-let obj = null;
-let formattedData = [];
-for (var i = 0; i < preparedData.length; i++) {
-  obj = preparedData[i];
-
-  if (!temp[obj.x]) {
-    temp[obj.x] = obj;
-  } else {
-    temp[obj.x].y += obj.y;
-  }
-}
-
-for (var prop in temp) formattedData.push(temp[prop]);
-
-const years = [];
-formattedData.map(el => {
-  years.push(el.x);
-});
+import { formattedData, years } from "./dataPrep";
 
 export default class Plot extends React.Component {
   render() {
