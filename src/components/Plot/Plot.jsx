b592c6e6fa6a4a229f525d706a18bb2f89116d70
Daniel Deutsch <deudan1010@gmail.com>
2017-11-27 16:13:08 +0100

    add regression library and use on data

--- src/components/Plot/Plot.jsx
+++ src/components/Plot/Plot.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import {
   FlexibleWidthXYPlot,
   MarkSeries,
+  LineSeries,
   HorizontalGridLines,
   VerticalGridLines,
   XAxis,
   YAxis
 } from "react-vis";
 import { formattedData, years } from "./dataPrep";
+import regressionData from "../../regression";
 
-const numbered = formattedData.map(el => {
-  return { x: parseFloat(el.x), y: el.y };
-});
-console.log(numbered);
+// const numbered = formattedData.map(el => {
+//   return { x: parseFloat(el.x), y: el.y };
+// });
+// console.log(numbered);
 
 class Plot extends React.Component {
   render() {
@@ -22,6 +24,7 @@ class Plot extends React.Component {
           <HorizontalGridLines />
           <VerticalGridLines />
           <MarkSeries className="mark-series-example" data={formattedData} />
+          <LineSeries data={regressionData} color="red" />
           <XAxis top={0} hideTicks tickValues={years} title="X" />
           <XAxis title="Year" tickFormat={v => v} />
           <YAxis title="Number of Marriages" />
---
+++ src/regression.js
@@ -0,0 +1,21 @@
+import regression from "regression";
+import { formattedData } from "./components/Plot";
+
+let regressionData = [];
+regressionData = formattedData.map(el => {
+  return [parseFloat(el.x), el.y];
+});
+
+const result = regression.linear(regressionData);
+const gradient = result.equation[0];
+const yIntercept = result.equation[1];
+
+regressionData = result.points.map(el =>{
+  return {
+    x: el[0],
+    y: el[1],
+  }
+});
+
+console.log(regressionData);
+export { regressionData as default, result };
