be88ebea516e313a698401806f96d5331fa36ebb
Daniel Deutsch <deudan1010@gmail.com>
2017-11-27 16:39:45 +0100

    update styling

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
         <Plot />
       </div>
