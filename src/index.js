d2168f17e5b27d0e6272887f4e65e463b36e7d6f
Daniel Deutsch <deudan1010@gmail.com>
2018-01-03 18:01:49 +0100

    add find polyfill

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+require('array.prototype.find');
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
