fc6cdf9735566f5a06f61f842a6aa4d36d94ea60
Daniel Deutsch <deudan1010@gmail.com>
2018-01-03 17:42:02 +0100

    remove polyfill

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-require('string.prototype.endswith');
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
